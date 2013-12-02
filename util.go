a115b568cbb2cf4a58ab8602ae8ceb056d84cf6a
nmeum <git-nmeum@8pit.net>
2013-12-02 15:31:22 +0100

    more custom date formats

--- util.go
+++ util.go
@@ -14,6 +14,9 @@ func parseDate(date string) (t time.Time, err error) {
 		time.RFC1123Z, time.RFC1123, time.RFC822Z,
 		time.RFC822, time.ANSIC, time.RFC3339,
 		time.RFC850, time.RubyDate, time.UnixDate,
+		"2 January 2006 15:04:05 -0700", "2 January 2006 15:04:05 MST",
+		"2 Jan 2006 15:04:05 -0700", "2 Jan 2006 15:04:05 MST",
+		"Mon, 2 Jan 2006 15:04:05 -0700", "Mon, 2 Jan 2006 15:04:05 MST",
 	}
 
 	for _, format := range formats {
