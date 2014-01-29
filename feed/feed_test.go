08615700fe659eae2b1a320cfcdcce371aafc90b
nmeum <git-nmeum@8pit.net>
2014-01-29 16:04:59 +0100

    added tests for feed.go

---
+++ feed/feed_test.go
@@ -0,0 +1,30 @@
+package feed
+
+import (
+	"testing"
+)
+
+type testpair struct {
+	Url   string
+	Type  string
+}
+
+func TestParse(t *testing.T) {
+	tests := []testpair{
+		{ "http://cyber.law.harvard.edu/rss/examples/sampleRss092.xml", "rss" },
+		{ "http://cyber.law.harvard.edu/rss/examples/rss2sample.xml", "rss" },
+		{ "http://www.heise.de/developer/rss/news-atom.xml", "atom" },
+		{ "http://blog.case.edu/news/feed.atom", "atom" },
+	}
+
+	for _, test := range tests {
+		feed, err := Parse(test.Url)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if feed.Type != test.Type {
+			t.Fatalf("Expected %q - got %q", test.Type, feed.Type)
+		}
+	}
+}
