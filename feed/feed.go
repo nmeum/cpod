716ed088d31a65a4cf070dbde4ab4d4831a48239
nmeum <git-nmeum@8pit.net>
2014-01-29 18:43:22 +0100

    move convert* functions to feed.go

--- feed/feed.go
+++ feed/feed.go
@@ -50,3 +50,39 @@ func Parse(url string) (f Feed, err error) {
 
 	return
 }
+
+func convertRss(r rss.Feed) (f Feed) {
+	f.Title = r.Title
+	f.Link = r.Link
+
+	for _, i := range r.Items {
+		item := Item{
+			Title:      i.Title,
+			Link:       i.Link,
+			Date:       i.PubDate,
+			Attachment: i.Enclosure.Url,
+		}
+
+		f.Items = append(f.Items, item)
+	}
+
+	return
+}
+
+func convertAtom(a atom.Feed) (f Feed) {
+	f.Title = a.Title
+	f.Link = findLink(a.Links).Href
+
+	for _, e := range a.Entries {
+		item := Item{
+			Title:      e.Title,
+			Link:       findLink(e.Links).Href,
+			Date:       e.Published,
+			Attachment: findAttachment(e.Links).Href,
+		}
+
+		f.Items = append(f.Items, item)
+	}
+
+	return
+}
