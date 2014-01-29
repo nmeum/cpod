c3c8f555a152ff3d7937145552b2749f73dc087d
nmeum <git-nmeum@8pit.net>
2014-01-29 20:36:20 +0100

    go fmt

--- feed/util_test.go
+++ feed/util_test.go
@@ -1,15 +1,15 @@
 package feed
 
 import (
-	"testing"
 	"github.com/nmeum/cpod/feed/atom"
+	"testing"
 )
 
 func TestFindLink(t *testing.T) {
 	links := []atom.Link{
-		{ "text/html", "http://example.com/my_link", "alternate" },
-		{ "audio/ogg", "http://example.com/my_foo", "alternate" },
-		{ "image/png", "http://example.com/image", "self" },
+		{"text/html", "http://example.com/my_link", "alternate"},
+		{"audio/ogg", "http://example.com/my_foo", "alternate"},
+		{"image/png", "http://example.com/image", "self"},
 	}
 
 	link := findLink(links)
@@ -20,9 +20,9 @@ func TestFindLink(t *testing.T) {
 
 func TestFindAttachment(t *testing.T) {
 	links := []atom.Link{
-		{ "text/html", "http://example.org/foo", "alternate" },
-		{ "text/html", "http://example.org/bar", "enclosure" },
-		{ "text/html", "http://example.org/baz", "" },
+		{"text/html", "http://example.org/foo", "alternate"},
+		{"text/html", "http://example.org/bar", "enclosure"},
+		{"text/html", "http://example.org/baz", ""},
 	}
 
 	link := findAttachment(links)
