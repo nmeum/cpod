c6eebb7e2e0237f9d8fa3024a992f8bcbb6cb372
Sören Tempel <soeren+git@soeren-tempel.net>
2015-02-03 21:23:32 +0100

    Document opml code

--- opml/opml.go
+++ opml/opml.go
@@ -1,3 +1,5 @@
+// Package opml implements a parser for OPML files.
+// See also: http://dev.opml.org/spec2.html
 package opml
 
 import (
@@ -12,19 +14,36 @@ import (
 const version = "2.0"
 
 type Opml struct {
-	XMLName  xml.Name  `xml:"opml"`
-	Version  string    `xml:"version,attr"`
-	Title    string    `xml:"head>title"`
-	Created  string    `xml:"head>dateCreated"`
+	// XML name.
+	XMLName xml.Name `xml:"opml"`
+
+	// OPML standard version implemented by this file.
+	Version string `xml:"version,attr"`
+
+	// Title of the OPML document.
+	Title string `xml:"head>title"`
+
+	// Time the document was created.
+	Created string `xml:"head>dateCreated"`
+
+	// Array of outlines, each represents a subscription.
 	Outlines []Outline `xml:"body>outline"`
 }
 
 type Outline struct {
+	// Text attribute, might contain HTML markup.
 	Text string `xml:"text,attr"`
+
+	// Type of file found at the outline URL.
 	Type string `xml:"type,attr"`
-	URL  string `xml:"xmlUrl,attr"`
+
+	// Arbitrary outline URL.
+	URL string `xml:"xmlUrl,attr"`
 }
 
+// Creates a new OPML document with the given title. However, this is
+// just syntax sugar. A file is only written after a call Save, it's the
+// callers responsibility to do so if desired.
 func Create(title string) (o *Opml) {
 	o = &Opml{
 		Version: version,
@@ -35,6 +54,7 @@ func Create(title string) (o *Opml) {
 	return
 }
 
+// Loads an existing OPML document located at the given path.
 func Load(path string) (o *Opml, err error) {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -48,6 +68,8 @@ func Load(path string) (o *Opml, err error) {
 	return
 }
 
+// Adds a new outline to the OPML document, even if the outline is
+// already a part of the document.
 func (o *Opml) Add(text, ftype, url string) {
 	outline := Outline{
 		Text: text,
@@ -58,6 +80,7 @@ func (o *Opml) Add(text, ftype, url string) {
 	o.Outlines = append(o.Outlines, outline)
 }
 
+// Writes an indented version of the OPML document to the given path.
 func (o *Opml) Save(path string) (err error) {
 	file, err := os.Create(path)
 	if err != nil {
@@ -65,7 +88,7 @@ func (o *Opml) Save(path string) (err error) {
 	}
 
 	defer file.Close()
-	data, err := xml.MarshalIndent(o, "", "  ")
+	data, err := xml.MarshalIndent(o, "", "\t")
 	if err != nil {
 		return
 	}
