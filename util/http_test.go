6c4ef8bc9e51c375f4ec942ca052af7561a214d3
Sören Tempel <soeren+git@soeren-tempel.net>
2015-02-08 21:17:13 +0100

    util: Test for get function

--- util/http_test.go
+++ util/http_test.go
@@ -1,6 +1,10 @@
 package util
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -24,5 +28,31 @@ func TestFilename(t *testing.T) {
 		if f != p.expected {
 			t.Fatalf("Expected %q - got %q", p.expected, f)
 		}
+	}
+}
+
+func TestGet(t *testing.T) {
+	expected := "Success\n"
+	th := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, expected)
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(th))
+	defer ts.Close()
+
+	resp, err := Get(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	result := string(data)
+	if result != expected {
+		t.Fatalf("Expected %q - got %q", expected, result)
 	}
 }
