d85af5b1e93e7cc5fd7223a34397264886183101
Sören Tempel <soeren+git@soeren-tempel.net>
2015-02-19 17:46:54 +0100

    store: Rewrite test

--- store/store_test.go
+++ store/store_test.go
@@ -1,6 +1,9 @@
 package store
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -10,25 +13,80 @@ func TestLoad(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if store.URLs[0] != "http://feeds.thisamericanlife.org/talpodcast" {
-		t.Fatalf("Expected %q - got %q", "http://feeds.thisamericanlife.org/talpodcast", store.URLs[0])
+	urls := []string{
+		"http://feeds.thisamericanlife.org/talpodcast",
+		"http://www.npr.org/rss/podcast.php?id=510294",
 	}
 
-	if store.URLs[1] != "http://www.npr.org/rss/podcast.php?id=510294" {
-		t.Fatalf("Expected %q - got %q", "http://www.npr.org/rss/podcast.php?id=510294", store.URLs[1])
+	for _, url := range urls {
+		if !store.Contains(url) {
+			t.Fail()
+		}
+	}
+}
+
+func TestAdd(t *testing.T) {
+	url := "http://example.com"
+	store := new(Store)
+
+	store.Add(url)
+	if !store.Contains(url) {
+		t.Fail()
+	}
+}
+
+func TestContains(t *testing.T) {
+	url := "http://foo.com"
+	store := &Store{"", []string{url}}
+
+	if !store.Contains(url) {
+		t.Fail()
+	}
+
+	if store.Contains("http://foo.bar") {
+		t.Fail()
 	}
 }
 
 func TestFetch(t *testing.T) {
-	store := &Store{
-		"",
-		[]string{"http://feeds.thisamericanlife.org/talpodcast"},
-	}
+	url := "http://feeds.thisamericanlife.org/talpodcast"
+	store := &Store{"", []string{url}}
 
 	channel := store.Fetch()
-	feed := <-channel
+	podcast := <-channel
 
-	if feed.Title != "This American Life" {
-		t.Fatalf("Expected %q - got %q", "This American Life", feed.Title)
+	if podcast.Error != nil {
+		t.Fatal(podcast.Error)
+	}
+
+	feed := podcast.Feed
+	if url != podcast.URL {
+		t.Fatalf("Expected %q - got %q", podcast.URL, url)
+	}
+
+	expected := "This American Life"
+	if feed.Title != expected {
+		t.Fatalf("Expected %q - got %q", expected, feed.Title)
+	}
+}
+
+func TestSave(t *testing.T) {
+	url := "http://example.io"
+	fp := filepath.Join(os.TempDir(), "testSave")
+
+	store := &Store{fp, []string{url}}
+	if err := store.Save(); err != nil {
+		t.Fatal(err)
+	}
+
+	defer os.Remove(fp)
+	data, err := ioutil.ReadFile(fp)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := url + "\n"
+	if string(data) != expected {
+		t.Fatalf("Expected %q - got %q", string(data), expected)
 	}
 }
