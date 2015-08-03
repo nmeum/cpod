73010239d7facd72995a5ae4c7cb57f1c2a0007e
Sören Tempel <soeren+git@soeren-tempel.net>
2015-08-03 18:36:00 +0200

    Revert "Set HTTP request timeout to 30 seconds"
    Couldn't find a decent default and thus decided to not set one.
    
    This reverts commit 69e8b2ed148a70a27b0d98320715cf41fa3cbea2.

--- util/http.go
+++ util/http.go
@@ -34,9 +34,6 @@ const (
 	// Number of maximal allowed redirects.
 	maxRedirects = 10
 
-	// Time limit for HTTP requests.
-	timeout = 30 * time.Second
-
 	// HTTP User-Agent.
 	useragent = "cpod"
 )
@@ -186,9 +183,8 @@ func doReq(req *http.Request) (resp *http.Response, err error) {
 	return
 }
 
-// headerClient returns a client witch a custom CheckRedirect function which
-// ensures that the given headers will be readded after a redirect. Furthermore
-// it also set a time limit for requests.
+// headerClient returns a client witch a custom CheckRedirect function
+// which ensures that the given headers will be readded after a redirect.
 func headerClient(headers http.Header) *http.Client {
 	redirectFunc := func(req *http.Request, via []*http.Request) error {
 		if len(via) >= maxRedirects {
@@ -199,5 +195,5 @@ func headerClient(headers http.Header) *http.Client {
 		return nil
 	}
 
-	return &http.Client{CheckRedirect: redirectFunc, Timeout: timeout}
+	return &http.Client{CheckRedirect: redirectFunc}
 }
