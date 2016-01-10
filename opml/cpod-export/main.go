908667773d1e18c18c68c2ac1830820ddfc3e551
Sören Tempel <soeren+git@soeren-tempel.net>
2016-01-10 02:39:41 +0100

    cpod-export: rewrite

--- opml/cpod-export/main.go
+++ opml/cpod-export/main.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2013-2015 Sören Tempel
+f/ Copyright (C) 2013-2016 Sören Tempel
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -16,12 +16,13 @@
 package main
 
 import (
+	"bufio"
+	"encoding/xml"
 	"fmt"
 	"github.com/nmeum/cpod/opml"
-	"github.com/nmeum/cpod/store"
 	"github.com/nmeum/cpod/util"
+	"github.com/nmeum/go-feedparser"
 	"os"
-	"path/filepath"
 	"sync"
 )
 
@@ -29,40 +30,62 @@ import (
 const title = "Podcast subscriptions"
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "USAGE: cpod-export FILE\n")
+	fmt.Fprintf(os.Stderr, "USAGE: cpod-export URLFILE\n")
 	os.Exit(1)
 }
 
+func createOpml(urls []string) *opml.OPML {
+	var wg sync.WaitGroup
+	opmlFile := opml.Create(title)
+
+	for _, url := range urls {
+		wg.Add(1)
+		go func(u string) {
+			defer wg.Done()
+			resp, err := util.Get(u)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+				return
+			}
+			defer resp.Body.Close()
+
+			feed, err := feedparser.Parse(resp.Body)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+			}
+
+			opmlFile.Add(feed.Title, feed.Type, u)
+		}(url)
+	}
+
+	wg.Wait()
+	return opmlFile
+}
+
 func main() {
-	if len(os.Args) <= 1 {
+	if len(os.Args) < 1 {
 		usage()
 	}
 
-	storeDir := filepath.Join(util.EnvDefault("XDG_CONFIG_HOME", ".config"), "cpod")
-	storage, err := store.Load(filepath.Join(storeDir, "urls"))
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var urls []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		urls = append(urls, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	data, err := xml.MarshalIndent(createOpml(urls), "", "\t")
 	if err != nil {
 		panic(err)
 	}
 
-	var wg sync.WaitGroup
-	opmlFile := opml.Create(title)
-
-	for cast := range storage.Fetch() {
-		wg.Add(1)
-		go func(p store.Podcast) {
-			defer wg.Done()
-			if p.Error != nil {
-				fmt.Fprintf(os.Stderr, "%s\n", p.Error)
-				return
-			}
-
-			feed := p.Feed
-			opmlFile.Add(feed.Title, feed.Type, p.URL)
-		}(cast)
-	}
-
-	wg.Wait()
-	if err = opmlFile.Save(os.Args[1]); err != nil {
-		panic(err)
-	}
+	fmt.Println(string(data))
 }
